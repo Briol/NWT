901ad6888ef22dffa41a5b24949fac8b89bf3967
Thomas Robveille <thomas.ro@hotmail.fr>
2018-04-11 16:18:17 +0200

    création des Classes pour l'algorightme - correction et implémentation d'objet

--- index.php
+++ index.php
@@ -1,3 +1,17 @@
-<?php ?>
+<?php
+include ("classes/Intervenant.php");
+include ("classes/Groupe.php");
+include ("classes/Salle.php");
+include ("classes/Reservation.php");
+?>
 
-<input type="date" name="reserve">
+<select>
+    <option><?php Intervenant?></option>
+</select>
+<select>
+    <option><?php Groupe?></option>
+</select>
+<select>
+    <option><?php Salle?></option>
+</select>
+<input type="date" name="dateChoisie" id="dateChoisie">
