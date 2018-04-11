e31fe1e687a1302d02208a4fe9bfe3e594a4d14a
Thomas Robveille <thomas.ro@hotmail.fr>
2018-04-11 16:22:29 +0200

    création des Classes pour l'algorightme - form de base fait

--- index.php
+++ index.php
@@ -5,13 +5,15 @@ include ("classes/Salle.php");
 include ("classes/Reservation.php");
 ?>
 
-<select>
-    <option><?php Intervenant?></option>
-</select>
-<select>
-    <option><?php Groupe?></option>
-</select>
-<select>
-    <option><?php Salle?></option>
-</select>
-<input type="date" name="dateChoisie" id="dateChoisie">
+<form>
+    <select>
+        <option><?php Intervenant?></option>
+    </select>
+    <select>
+        <option><?php Groupe?></option>
+    </select>
+    <select>
+        <option><?php Salle?></option>
+    </select>
+    <input type="date" name="dateChoisie" id="dateChoisie">
+</form>
